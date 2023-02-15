9c71f3731506f2ccbbdfa6ce60a06b9e5e2434e1
Bod1985 <crwblyth@gmail.com>
2023-02-15 15:21:48 +0000

    stop containers, compare to ignore list, restart

--- backup.py
+++ backup.py
@@ -4,6 +4,7 @@ from subprocess import Popen, PIPE
 import docker
 
 ignore_list = ['docker-backup','portainer']
+stopped_container_ids = []
 client = docker.APIClient(base_url='unix://var/run/docker.sock')
 
 running_containers = client.containers()
@@ -11,12 +12,13 @@ running_containers = client.containers()
 print(running_containers)
 
 for container in running_containers:
-    if container.name not in ignore_list:
-        client.stop(container)
+    if container['Names'][0].strip('/') not in ignore_list:
+        client.stop(container['Id'])
+        stopped_container_ids.add('Id')
 
 process = Popen(['cp', '-r', '/source/*', '/dest'], stdout=PIPE, stderr=PIPE)
 stdout, stderr = process.communicate()
+print(stdout)
 
-for container in running_containers:
-    if container.name not in ignore_list:
-        client.start(container)
+for container_id in stopped_container_ids:
+        client.start(container_id)
