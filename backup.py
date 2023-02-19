2e6c4dc9c5872685c8ca5838a313c24273056ab7
Bod1985 <crwblyth@gmail.com>
2023-02-19 22:47:28 +0000

    Update backup.py

--- backup.py
+++ backup.py
@@ -96,8 +96,9 @@ def clean_old_backups():
         if os.path.isdir(folder):
             file_as_date = file.split('-')
             file_as_date = datetime.date(int(file_as_date[0]),int(file_as_date[1]),int(file_as_date[2]))
-            print('file as date', type(file_as_date), 'past date', type(get_past_date(days_ago)))
-            if file_as_date < get_past_date(days_ago):
+            past_as_date = get_past_date(days_ago).split('-')
+            past_as_date = datetime.date(int(past_as_date[0]),int(past_as_date[1]),int(past_as_date[2]))
+            if file_as_date < past_as_date:
                 send_notification('Docker-Backup',f'Removing {file} as it\'s older than {cutoff}')
     return
 
